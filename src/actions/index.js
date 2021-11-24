9e0b4512fa6fcb756a50c8d529aa70e0a516c021
john <johnjamero007@gmail.com>
2021-11-24 16:35:37 +0800

    initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+import jsonPlaceholder from '../api/jsonPlaceholder';
+
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get('/posts');
+
+  dispatch({ type: 'FETCH_POSTS', payload: response });
+};
