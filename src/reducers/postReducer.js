add908a2e824d85b24354f0517930b8623b781c6
john <johnjamero007@gmail.com>
2021-11-25 09:00:39 +0800

    adding actions and reducers

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,3 +1,9 @@
-export default () => {
-  return 123;
+/* eslint-disable import/no-anonymous-default-export */
+export default (state = [], action) => {
+  switch (action.type) {
+    case "FETCH_POSTS":
+      return action.payload;
+    default:
+      return state;
+  }
 };
